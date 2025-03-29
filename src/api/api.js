c0286ef477b3724e831a6c075f254a1b9b22ede9
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-03-29 20:06:23 +0300

    some work

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import Cookies from 'js-cookie'
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+const baseUrl = "http://192.168.1.42:8000/api";
 
 const csrftoken = Cookies.get('csrftoken');
-// console.log(csrftoken);
+console.log(csrftoken);
 
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -10,20 +13,10 @@ axios.defaults.xsrfCookieName = "csrftoken";
 export const instance = axios.create({
     baseURL: "http://192.168.1.42:8000",
     withCredentials: true,
+
     headers:{
-         "csrftoken": csrftoken,
-    }
+        'X-CSRFTOKEN': csrftoken,
+    },
 
 
-})
-
-export async function getData() {
-    return await instance.get('api/get_data')
-}
-export async function loginApi(data) {
-    return await instance.post("/api/login/", data)
-}
-export async function logoutApi(data) {
-    return await instance.get("/api-auth/logout/")
-}
-
+});
