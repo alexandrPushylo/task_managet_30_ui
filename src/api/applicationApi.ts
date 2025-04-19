bccd512024eb0cd3e99f1cec058e015f1835527c
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-04-19 11:27:29 +0300

    Update applicationApi.ts

--- src/api/applicationApi.ts
+++ src/api/applicationApi.ts
@@ -1,38 +1,38 @@
 import {instance} from "./api";
 import {useQuery} from "@tanstack/react-query";
 import {PostTitle} from "../assets/assets";
+import {useAppSelector} from "../store/store";
+import {applicationSlice} from "../store/slices/applicationSlice";
 
-interface IApplicationData {
-    today?: string;
-    "current_weekday"?: string;
-    "prev_work_day"?: string;
-    "next_work_day"?: string;
-    "weekday"?: string;
-    "view_mode"?: "view_mode_future" | "view_mode_current" | "view_mode_archive"
-    "accept_mode"?: boolean;
-    "is_authenticated": boolean;
-    "current_user"?: {
-        id: number;
-        username: string;
-        first_name: string | undefined;
-        last_name: string | undefined;
-        password: string;
-        telephone: string | undefined;
-        isArchive: boolean;
-        post: string;
-        supervisor_user_id: number | undefined;
-        telegram_id_chat: string | undefined;
-    };
+
+interface IToday{
+    date: string;
+    weekday: string;
+    day: number;
+    month: number;
+    year: number;
+    month_name: string;
+    status: boolean;
+}
+interface ICurrentDate{
+    date: string;
+    weekday: string;
+    day: number;
+    month: number;
+    year: number;
+    month_name: string;
+    status: boolean;
 }
 
-interface appDataDto {
-    today?: string;
-    "current_weekday"?: string;
-    "prev_work_day"?: string;
-    "next_work_day"?: string;
-    "weekday"?: string;
-    "view_mode"?: "view_mode_future" | "view_mode_current" | "view_mode_archive"
-    "accept_mode"?: boolean;
+export interface appDataDto {
+    today: IToday;
+    current_date: ICurrentDate;
+    current_weekday?: string;
+    prev_work_day?: string;
+    next_work_day?: string;
+    weekday?: string;
+    view_mode?: "view_mode_future" | "view_mode_current" | "view_mode_archive"
+    accept_mode?: boolean;
 }
 
 interface currentUserDto {
@@ -47,17 +47,37 @@ interface currentUserDto {
     supervisor_user_id: number | undefined;
     telegram_id_chat: string | undefined;
 }
+export interface WorkDaysDto{
+    id: number;
+    date: string;
+    day: number;
+    month: number;
+    status: boolean;
+    weekday: string;
+}
 
 
-export function useAppData(init?: { enabled: boolean }) {
-    const {data: appData, isLoading, isError, refetch} = useQuery({
-        queryKey: ['appData'],
+export function useGetWorkDays() {
+    const {data: workDays, isLoading, isError} = useQuery({
+        queryKey: ['workDaysPaginator'],
         queryFn: async (meta) => {
-            const response = await instance.get<appDataDto>('api/get_data', {signal: meta.signal});
+            const response = await instance.get<WorkDaysDto[]>('/api/get_work_day/', {signal: meta.signal});
+            return response.data;
+        },
+    })
+    return {workDays, isLoading, isError};
+}
+export function useAppData(init?: { enabled?: boolean, current_day?:string }) {
+    const currentDay = useAppSelector(applicationSlice.selectors.selectCurrentDay);
+    const url_current_day = init?.current_day?`?current_day=${init?.current_day}`: `?current_day=${currentDay}`;
+    const {data: appData, isLoading, isError, refetch} = useQuery({
+        queryKey: ['appData', init?.current_day],
+        queryFn: async (meta) => {
+            const response = await instance.get<appDataDto>(`api/get_data/${url_current_day}`, {signal: meta.signal});
             return response.data;
         },
         // ...init,
-        enabled: init?.enabled ?? false
+        enabled: init?.enabled ?? true
 
     })
     return {appData, isLoading, isError, refetch};
