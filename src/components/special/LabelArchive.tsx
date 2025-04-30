3b5b5ce3c964da74f11cfca9bdac13cfa7178738
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-04-30 21:40:51 +0300

    Create LabelArchive.tsx

---
+++ src/components/special/LabelArchive.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LabelArchive() {
+    return <div
+        style={{textAlign: 'center'}}
+    >
+        <span className="m-0 fs-4">Архив</span>
+    </div>
+
+}
